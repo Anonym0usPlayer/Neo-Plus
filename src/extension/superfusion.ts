734313475d71583e96cd7e38b9da854b1d89b35a
QYLexpired <1525124972@qq.com>
2026-06-12 22:58:43 +0800

    1.1.4

--- src/extension/superfusion.ts
+++ src/extension/superfusion.ts
@@ -1,13 +1,11 @@
 import { isMobile } from '../modules/env';
 import { saveConfig, loadConfig } from '../main/data';
 import type { Config } from '../main/data';
-let destroyed = false;
 export function initSuperFusion(): void {
   if (isMobile()) return;
   loadConfig().then((config) => {
     if (config['super-fusion'] === true) {
       document.documentElement.classList.add('neo-extension-superfusion');
-      destroyed = false;
     }
   });
 }
@@ -22,10 +20,8 @@ export function onSuperFusionClick(): void {
   } else {
     htmlEl.classList.add('neo-extension-superfusion');
     saveConfig({ 'super-fusion': true } as Partial<Config>);
-    destroyed = false;
   }
 }
 export function destroySuperFusion(): void {
-  destroyed = true;
   document.documentElement?.classList.remove('neo-extension-superfusion');
 }
