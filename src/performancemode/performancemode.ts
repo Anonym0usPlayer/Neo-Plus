d19539e90b95ba88aaaf9942f3e7019bacf4c471
QYLexpired <1525124972@qq.com>
2026-07-07 00:30:24 +0800

    update

--- src/performancemode/performancemode.ts
+++ src/performancemode/performancemode.ts
@@ -1,3 +1,5 @@
+import { showMessage } from 'siyuan';
+import { getPlugin } from '../main/guard';
 import { saveConfig, loadConfig } from '../main/data';
 import type { Config } from '../main/data';
 function withViewTransition(callback: () => void): void {
@@ -24,6 +26,7 @@ export function onPerformanceClick(): void {
     } else {
       htmlEl.classList.add('neo-performance-mode');
       saveConfig({ 'performance': true } as Partial<Config>);
+      showMessage(getPlugin().i18n.performanceMessage, 3000);
     }
   });
 }
