f5d805c924217f782419df6e4d186ef78fb671f0
QYLexpired <1525124972@qq.com>
2026-08-22 11:05:56 +0800

    update

--- src/modules/svgfilter.ts
+++ src/modules/svgfilter.ts
@@ -22,8 +22,16 @@ function injectSvgFilter(): void {
         <feMergeNode in="SourceGraphic"/>
       </feMerge>
     </filter>
-    <filter id="neo-brutalism-emoji" x="-50%" y="-50%" width="200%" height="200%">
+    <filter id="neo-brutalism-emoji-light" x="-50%" y="-50%" width="200%" height="200%">
       <feColorMatrix in="SourceGraphic" type="matrix" values="0.25 0 0 0 0 0 0.25 0 0 0 0 0 0.25 0 0 0 0 0 1 0" result="darkened"/>
+      <feOffset in="darkened" dx="8" dy="10" result="shadow"/>
+      <feMerge>
+        <feMergeNode in="shadow"/>
+        <feMergeNode in="SourceGraphic"/>
+      </feMerge>
+    </filter>
+    <filter id="neo-brutalism-emoji-dark" x="-50%" y="-50%" width="200%" height="200%">
+      <feColorMatrix in="SourceGraphic" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0" result="darkened"/>
       <feOffset in="darkened" dx="8" dy="10" result="shadow"/>
       <feMerge>
         <feMergeNode in="shadow"/>
